74af51ef7a92423997ba69aceaaa351bbff204b8
colin-lallauret <colinlallauret1@gmail.com>
2024-10-23 09:08:07 +0200

    lab1 completed

--- src/lab1.js
+++ src/lab1.js
@@ -9,3 +9,12 @@
   - console.log process
   - console.log window
  */
+
+const elements = ["element1", "element2", "element3"];
+console.table(elements);
+console.log(__dirname);
+console.log(__filename);
+console.log(require);
+console.log(module);
+console.log(process);
+console.log(window);
